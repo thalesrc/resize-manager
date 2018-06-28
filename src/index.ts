e81bf82f53a7746d1c8faf3710c099ef2a5dc329
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2018-06-28 21:13:35 +0300

    default resizeManager instance

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import { ResizeManager } from './resize-manager';
+
 export { GTResizeObserver as ResizeObserver } from "./resize-observer";
 export { ResizeManager } from "./resize-manager";
+
+export default new ResizeManager();
